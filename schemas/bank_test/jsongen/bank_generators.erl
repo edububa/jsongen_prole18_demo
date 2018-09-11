9c4da3304626ec2704799adea0b0ca1dd097a88c
Luis <edububa@gmail.com>
2018-09-12 00:33:18 +0200

    Delete bank_generators.erl

--- schemas/bank_test/jsongen/bank_generators.erl
+++
@@ -1,9 +0,0 @@
--module(bank_generators).
-
--include_lib("eqc/include/eqc.hrl").
-
--compile(export_all).
-
-gen_user(_, _) ->
-    UserInt = gen_server_users:new(),
-    eqc_gen:elements(integer_to_list(UserInt)).
