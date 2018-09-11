10cec5a2b378c501a4aa0719917d086f2facb5ec
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2018-09-11 14:12:44 +0200

    Fixes and transfer operation

--- api2/auth_bank.py
+++ api2/auth_bank.py
@@ -17,6 +17,8 @@ parser_operation.add_argument('quantity', type=int)
 parser_account = reqparse.RequestParser()
 parser_account.add_argument('operation', type=str)
 parser_account.add_argument('quantity', type=int)
+parser_account.add_argument('to_user', type=str)
+parser_account.add_argument('to_account', type=str)
 
 parser_create_account = reqparse.RequestParser()
 parser_create_account.add_argument('account_name', type=str)
@@ -30,7 +32,10 @@ class SingleAccount(Resource):
     def get(self, user, account):
         if user not in balances:
             return error(404, "user {} not found".format(user)), 404
-        return {"balance": balances[user] }
+        if account not in balances[user]:
+            return error(404, "account {} of user {} not found".format(acccount, user)), 404
+        return {"account_name": account,
+                "balance": balances[user][account]}
 
     def post(self, user, account):
         args = parser_account.parse_args()
@@ -47,7 +52,6 @@ class SingleAccount(Resource):
             return error(404, "account {} of user {} not found".format(account, user)), 404
         if operation not in operations:
             return error(409, "operation {} not recognized".format(operation)),  409
-        print("Operation:\t", operation, operation == "deposit")
         if operation == "deposit":
             balances[user][account] += quantity
             print(balances)
@@ -55,7 +59,22 @@ class SingleAccount(Resource):
             if (balances[user][account] - quantity) < 0:
                 return error(409, "Aborted. Negative balance after withdraw"),  409
             balances[user][account] -= quantity
-        return {"account": account,
+        if operation == "transfer":
+            to_user = args['to_user']
+            if to_user is None:
+                return error(422, "to_user: Destination user necessary for transfer operation"), 422
+            to_account = args['to_account']
+            if to_account is None:
+                return error(422, "to_account: Destination user necessary for transfer operation"), 422
+            if to_user not in balances:
+                return error(404, "user {} not found".format(to_user)), 404
+            if to_account not in balances[user]:
+                return error(404, "account {} of user {} not found".format(to_account, to_user)), 404
+            if (balances[user][account] - quantity) < 0:
+                return error(409, "Aborted. Negative balance after withdraw"),  409
+            balances[user][account] -= quantity
+            balances[to_user][to_account] += quantity
+        return {"account_name": account,
                 "balance": balances[user][account]}, 201
 
 
@@ -84,6 +103,8 @@ class Balances(Resource):
     def post(self):
         args = parser_post_user.parse_args()
         user = args['user']
+        if user == "":
+            return error(422, "Empty string not permitted"), 422
         password = args['password']
         if user is not None and password is not None:
             user = user.replace(" ", "-")
@@ -91,7 +112,7 @@ class Balances(Resource):
                 balances[user] = {}
                 return {"user" : user}, 201, {'Content-Location': request.base_url + user}
             else:
-                return erro(409, "user already exists"), 409
+                return error(409, "user already exists"), 409
         else:
             return error(422, "user and/or password missing"), 422
 
