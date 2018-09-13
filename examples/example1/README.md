c5ad9e5b999e15bfe4be1e4998a77c896e7c6907
Luis Eduardo <edububa@gmail.com>
2018-09-13 23:51:25 +0200

    slides -> example2; examples -> {example1, example2}

---
+++ examples/example1/README.md
@@ -0,0 +1,9 @@
+example1
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ examples/example2/src/gen_server_users.erl
@@ -0,0 +1,26 @@
+-module(gen_server_users).
+
+-behaviour(gen_server).
+
+-export([init/1]).
+-export([handle_call/3, terminate/2]).
+-export([start/0, new/0]).
+
+init(_) -> {ok, 0}.
+
+handle_call(Msg, _From, State) ->
+  case Msg of
+    {new} ->
+      {reply, State + 1, State + 1}
+  end.
+
+terminate(_,_) -> ok.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+start() ->
+  {ok, Pid} = gen_server:start(?MODULE,[],[]),
+  register(?MODULE, Pid),
+  ?MODULE.
+
+new() -> gen_server:call(?MODULE, {new}).
