65c2bbb0096b73377bff3b1e0ef76d3bb21a1a3b
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2018-09-18 12:24:44 +0200

    /consult/ -> /balance/

--- slides/code/postcondition_model_state.erl
+++ slides/code/postcondition_model_state.erl
@@ -5,7 +5,7 @@ postcondition_model_state(Operation, ModelState,
                         end,
                         ModelState#state.accounts)) == []
     and case Operation of
-          "consult account" ->
+          "balance account" ->
             case {proplists:lookup(<<"accountid">>, Values),
                   proplists:lookup(<<"balance">>, Values)} of
               {{_, AccountId}, {_, Balance}} ->
